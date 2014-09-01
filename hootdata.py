738f68012c94c912242fe69fc9615ae2937d282c
CallaJun <maracondo@aol.com>
2014-09-01 13:43:20 -0400

    PemEast added

--- hootdata.py
+++ hootdata.py
@@ -7,24 +7,24 @@ PEMEAST_HA = {
 
 
 PEMEAST_PM = [
-    'Katie Guye',
-    'Palak Bhandari'
+    'Margaret Sumner-Moore',
+    'Rasha Younes'
 ]
 
 PEMEAST_C1 = [
-	'Jane Rossman',
-	'Rachael Koone',
-	'Priyanka Dutta'
+    'Callie Lopshire-Bratt',
+	'Emily Drummond',
+	'Abigail Hoyt'
 ]
 
 PEMEAST_C2 = [
-	'Erica Rice',
-	'Maya Berrol-Young'
+	'Hannah Henderson-Charnow',
+	'Sofia Chavez'
 ]
 
 PEMEAST_C3 = [
-	'Carol Bowe',
-	'Glynn Siobhan'
+	'Hannah Rifkin',
+	'Abby Crum'
 ]
 
 PEMWEST_HA = {
@@ -35,8 +35,8 @@ PEMWEST_HA = {
 
 
 PEMWEST_PM = [
-    'Katie Guye',
-    'Palak Bhandari'
+    'Carol Bowe',
+    'Glynn Siobhan'
 ]
 
 #PEMWEST
@@ -57,37 +57,53 @@ PEMWEST_C3 = [
 ]
 
 EMAILS = {
+    'Abigail Hoyt': 'ahoyt@brynmawr.edu',
+    'Abby Crum': 'acrum@brynmawr.edu',
 	'Ahmad Shamial': 'siahmad@brynmawr.edu',
 	'Akthar Hazara': 'hakthar@brynmawr.edu',
     'Anna Sargeant': 'asargeant@brynmawr.edu',
+    'Callie Lopshire-Bratt': 'clopshireb@brynmawr.edu',
     'Carol Bowe': 'cbowe@brynmawr.edu',
+    'Emily Drummond': 'edrummond@brynmawr.edu',
     'Erica Rice': 'earice@brynmawr.edu',
     'Glynn Siobhan': 'sglynn@brynmawr.edu',
+    'Hannah Henderson-Charnow': 'hchenderso@brynmawr.edu',
+    'Hannah Rifkin': 'hrifkin@brynmawr.edu',
     'Jane Rossman': 'jrossman@brynmawr.edu',
     'Kailina Mastroianni': 'kmastroian@brynmawr.edu',
     'Katie Guye': 'kguye@brynmawr.edu',
     'Maya Berrol-Young': 'mberrolyou@brynmawr.edu',
+    'Margaret Sumner-Moore': 'msumnermoo@brynmawr.edu',
     'Palak Bhandari': 'pbhandari@brynmawr.edu',
     'Rachael Koone': 'rkoone@brynmawr.edu',
     'Rasha Younes': 'ryounes@brynmawr.edu',
+    'Sofia Chavez': 'schavez@brynmawr.edu',
     'Sophia Dauria': 'sdauria@brynmawr.edu',
     'Priyanka Dutta': 'pdutta@brynmawr.edu'
 }
 
 ROOMS = {
+    'Abigail Hoyt': 'Pem East 116',
+    'Abby Crum': 'Pem East 320',
 	'Ahmad Shamial': 'Pem West 323',
 	'Akthar Hazara': 'Pem West 207',
 	'Anna Sargeant': 'Pem West 115',
 	'Carol Bowe': 'Pem West 324',
+    'Callie Lopshire-Bratt': 'Pem East 108',
+    'Emily Drummond': 'Pem East 105',
 	'Erica Rice': 'Pem West 206',
 	'Glynn Siobhan': 'Pem West 319',
+    'Hannah Henderson-Charnow': 'Pem East 210',
+    'Hannah Rifkin': 'Pem East 320',
 	'Jane Rossman': 'Pem West 103',
 	'Kailina Mastroianni': 'Pem East 322',
     'Katie Guye': 'Pem West 219',
     'Maya Berrol-Young': 'Pem West 217',
+    'Margaret Sumner-Moore': 'Pem East 103',
     'Palak Bhandari': 'Pem West 204',
     'Rachael Koone': 'Pem West 114',
     'Rasha Younes': 'Pem East 120',
+    'Sofia Chavez': 'Pem East 219',
     'Sophia Dauria': 'Pem East 222',
     'Priyanka Dutta': 'Pem West 107'
 }
