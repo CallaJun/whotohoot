c4f193d308b19b7bd7bda42997bf92cadee58b81
CallaJun <maracondo@aol.com>
2014-09-01 14:09:37 -0400

    Brecon

--- hootdata.py
+++ hootdata.py
@@ -1,3 +1,35 @@
+#BRECON
+BRECON_HA = {
+    '1': 'Grace Baelen-King',
+    '2': 'Claire Craig',
+    '3': 'Jessica Sisco',
+    '4': 'Libby Wilson'
+}
+
+BRECON_PM = [
+    'Phoebe Jordan',
+    'Sophie Byvik'
+]
+
+BRECON_C1 = [
+    'Christina Ulowetz'
+]
+
+BRECON_C2 = [
+    'Alena Klindziuk',
+    'Stella Cai'
+]
+
+BRECON_C3 = [
+    'Chutong Zhu',
+    'Kalina Kostyszyn'
+]
+
+BRECON_C4 = [
+    'Austen Hamilton',
+    'Marcia Manzone'
+]
+
 #PEMEAST
 PEMEAST_HA = {
     '1': 'Rasha Younes',
@@ -27,6 +59,7 @@ PEMEAST_C3 = [
 	'Abby Crum'
 ]
 
+#PEMWEST
 PEMWEST_HA = {
     '1': 'Anna Sargeant',
     '2': 'Akthar Hazara',
@@ -39,7 +72,6 @@ PEMWEST_PM = [
     'Glynn Siobhan'
 ]
 
-#PEMWEST
 PEMWEST_C1 = [
 	'Jane Rossman',
 	'Rachael Koone',
@@ -61,24 +93,37 @@ EMAILS = {
     'Abby Crum': 'acrum@brynmawr.edu',
 	'Ahmad Shamial': 'siahmad@brynmawr.edu',
 	'Akthar Hazara': 'hakthar@brynmawr.edu',
+    'Alena Klindziuk': 'aklindziuk@brynmawr.edu',
     'Anna Sargeant': 'asargeant@brynmawr.edu',
+    'Austen Hamilton': 'ahamilto01@brynmawr.edu',
     'Callie Lopshire-Bratt': 'clopshireb@brynmawr.edu',
     'Carol Bowe': 'cbowe@brynmawr.edu',
+    'Christina Ulowetz': 'culowetz@brynmawr.edu',
+    'Chutong Zhu': 'czhu@brynmawr.edu',
+    'Claire Craig': 'cacraig@brynmawr.edu',
     'Emily Drummond': 'edrummond@brynmawr.edu',
     'Erica Rice': 'earice@brynmawr.edu',
     'Glynn Siobhan': 'sglynn@brynmawr.edu',
+    'Grace Baelen-King': 'ebaelenkin@brynmawr.edu',
     'Hannah Henderson-Charnow': 'hchenderso@brynmawr.edu',
     'Hannah Rifkin': 'hrifkin@brynmawr.edu',
     'Jane Rossman': 'jrossman@brynmawr.edu',
+    'Jessica Sisco': 'jsisco@brynmawr.edu',
     'Kailina Mastroianni': 'kmastroian@brynmawr.edu',
+    'Kalina Kostyszyn': 'kkostyszyn@brynmawr.edu',
     'Katie Guye': 'kguye@brynmawr.edu',
-    'Maya Berrol-Young': 'mberrolyou@brynmawr.edu',
+    'Libby Wilson': 'ewwilson@brynmawr.edu',
+    'Marcia Manzone': 'mmanzone@brynmawr.edu',
     'Margaret Sumner-Moore': 'msumnermoo@brynmawr.edu',
+    'Maya Berrol-Young': 'mberrolyou@brynmawr.edu',
     'Palak Bhandari': 'pbhandari@brynmawr.edu',
+    'Phoebe Jordan': 'pejordan@brynmawr.edu',
     'Rachael Koone': 'rkoone@brynmawr.edu',
     'Rasha Younes': 'ryounes@brynmawr.edu',
     'Sofia Chavez': 'schavez@brynmawr.edu',
+    'Sophie Byvik': 'sbyvik@brynmawr.edu',
     'Sophia Dauria': 'sdauria@brynmawr.edu',
+    'Stella Cai': 'ycai01@brynmawr.edu',
     'Priyanka Dutta': 'pdutta@brynmawr.edu'
 }
 
@@ -87,23 +132,36 @@ ROOMS = {
     'Abby Crum': 'Pem East 320',
 	'Ahmad Shamial': 'Pem West 323',
 	'Akthar Hazara': 'Pem West 207',
+    'Alena Klindziuk': 'Brecon 214',
 	'Anna Sargeant': 'Pem West 115',
+    'Austen Hamilton': 'Brecon 404',
 	'Carol Bowe': 'Pem West 324',
     'Callie Lopshire-Bratt': 'Pem East 108',
+    'Christina Ulowetz': 'Brecon 105',
+    'Chutong Zhu': 'Brecon 306',
+    'Claire Craig': 'Brecon 209',
     'Emily Drummond': 'Pem East 105',
 	'Erica Rice': 'Pem West 206',
 	'Glynn Siobhan': 'Pem West 319',
+    'Grace Baelen-King': 'Brecon 106',
     'Hannah Henderson-Charnow': 'Pem East 210',
     'Hannah Rifkin': 'Pem East 320',
 	'Jane Rossman': 'Pem West 103',
+    'Jessica Sisco': 'Brecon 305',
 	'Kailina Mastroianni': 'Pem East 322',
+    'Kalina Kostyszyn': 'Brecon 315',
     'Katie Guye': 'Pem West 219',
-    'Maya Berrol-Young': 'Pem West 217',
+    'Libby Wilson': 'Brecon 410',
+    'Marcia Manzone': 'Brecon 409',
     'Margaret Sumner-Moore': 'Pem East 103',
+    'Maya Berrol-Young': 'Pem West 217',
     'Palak Bhandari': 'Pem West 204',
+    'Phoebe Jordan': 'Brecon 103',
     'Rachael Koone': 'Pem West 114',
     'Rasha Younes': 'Pem East 120',
     'Sofia Chavez': 'Pem East 219',
+    'Sophie Byvik': 'Brecon 406',
     'Sophia Dauria': 'Pem East 222',
+    'Stella Cai': 'Brecon 201',
     'Priyanka Dutta': 'Pem West 107'
 }
--- main.py
+++ main.py
@@ -41,8 +41,132 @@ class HootHandler(webapp2.RequestHandler):
                     	'floor' : self.request.get('floor'),
                        }
 
+    #BRECON FIRST
+    if self.request.get('dorm')=='Brecon' and self.request.get('floor')=='1':
+        template_values['halladvisor'] = hootdata.BRECON_HA[self.request.get('floor')]
+        template_values['halladvisoremail'] = hootdata.EMAILS[hootdata.BRECON_HA[self.request.get('floor')]]
+        template_values['halladvisorroom'] = hootdata.ROOMS[hootdata.BRECON_HA[self.request.get('floor')]]
+        customspeople = []
+        customspeopleemails = []
+        customspeoplerooms = []
+        peermentors = []
+        peermentorsemails = []
+        peermentorsrooms = []
+        #Custom people
+        for person in range(0,len(hootdata.BRECON_C1)):
+            customspeople.append(hootdata.BRECON_C1[person])
+        template_values['customspeople'] = customspeople
+        for person in customspeople:
+            customspeopleemails.append(hootdata.EMAILS[person])
+        template_values['customspeopleemails'] = customspeopleemails
+        for person in customspeople:
+            customspeoplerooms.append(hootdata.ROOMS[person])
+        template_values['customspeoplerooms'] = customspeoplerooms
+        #Peer mentors
+        for person in range(0,len(hootdata.BRECON_PM)):
+            peermentors.append(hootdata.BRECON_PM[person])
+        template_values['peermentors'] = peermentors
+        for person in peermentors:
+            peermentorsemails.append(hootdata.EMAILS[person])
+        template_values['peermentorsemails'] = peermentorsemails
+        for person in peermentors:
+            peermentorsrooms.append(hootdata.ROOMS[person])
+
+    #BRECON SECOND
+    elif self.request.get('dorm')=='Brecon' and self.request.get('floor')=='2':
+        template_values['halladvisor'] = hootdata.BRECON_HA[self.request.get('floor')]
+        template_values['halladvisoremail'] = hootdata.EMAILS[hootdata.BRECON_HA[self.request.get('floor')]]
+        template_values['halladvisorroom'] = hootdata.ROOMS[hootdata.BRECON_HA[self.request.get('floor')]]
+        customspeople = []
+        customspeopleemails = []
+        customspeoplerooms = []
+        peermentors = []
+        peermentorsemails = []
+        peermentorsrooms = []
+        #Custom people
+        for person in range(0,len(hootdata.BRECON_C2)):
+            customspeople.append(hootdata.BRECON_C2[person])
+        template_values['customspeople'] = customspeople
+        for person in customspeople:
+            customspeopleemails.append(hootdata.EMAILS[person])
+        template_values['customspeopleemails'] = customspeopleemails
+        for person in customspeople:
+            customspeoplerooms.append(hootdata.ROOMS[person])
+        template_values['customspeoplerooms'] = customspeoplerooms
+        #Peer mentors
+        for person in range(0,len(hootdata.BRECON_PM)):
+            peermentors.append(hootdata.BRECON_PM[person])
+        template_values['peermentors'] = peermentors
+        for person in peermentors:
+            peermentorsemails.append(hootdata.EMAILS[person])
+        template_values['peermentorsemails'] = peermentorsemails
+        for person in peermentors:
+            peermentorsrooms.append(hootdata.ROOMS[person])
+
+    #BRECON THIRD
+    elif self.request.get('dorm')=='Brecon' and self.request.get('floor')=='3':
+        template_values['halladvisor'] = hootdata.BRECON_HA[self.request.get('floor')]
+        template_values['halladvisoremail'] = hootdata.EMAILS[hootdata.BRECON_HA[self.request.get('floor')]]
+        template_values['halladvisorroom'] = hootdata.ROOMS[hootdata.BRECON_HA[self.request.get('floor')]]
+        customspeople = []
+        customspeopleemails = []
+        customspeoplerooms = []
+        peermentors = []
+        peermentorsemails = []
+        peermentorsrooms = []
+        #Custom people
+        for person in range(0,len(hootdata.BRECON_C3)):
+            customspeople.append(hootdata.BRECON_C3[person])
+        template_values['customspeople'] = customspeople
+        for person in customspeople:
+            customspeopleemails.append(hootdata.EMAILS[person])
+        template_values['customspeopleemails'] = customspeopleemails
+        for person in customspeople:
+            customspeoplerooms.append(hootdata.ROOMS[person])
+        template_values['customspeoplerooms'] = customspeoplerooms
+        #Peer mentors
+        for person in range(0,len(hootdata.BRECON_PM)):
+            peermentors.append(hootdata.BRECON_PM[person])
+        template_values['peermentors'] = peermentors
+        for person in peermentors:
+            peermentorsemails.append(hootdata.EMAILS[person])
+        template_values['peermentorsemails'] = peermentorsemails
+        for person in peermentors:
+            peermentorsrooms.append(hootdata.ROOMS[person])
+
+    #BRECON FOURTH
+    elif self.request.get('dorm')=='Brecon' and self.request.get('floor')=='4':
+        template_values['halladvisor'] = hootdata.BRECON_HA[self.request.get('floor')]
+        template_values['halladvisoremail'] = hootdata.EMAILS[hootdata.BRECON_HA[self.request.get('floor')]]
+        template_values['halladvisorroom'] = hootdata.ROOMS[hootdata.BRECON_HA[self.request.get('floor')]]
+        customspeople = []
+        customspeopleemails = []
+        customspeoplerooms = []
+        peermentors = []
+        peermentorsemails = []
+        peermentorsrooms = []
+        #Custom people
+        for person in range(0,len(hootdata.BRECON_C4)):
+            customspeople.append(hootdata.BRECON_C4[person])
+        template_values['customspeople'] = customspeople
+        for person in customspeople:
+            customspeopleemails.append(hootdata.EMAILS[person])
+        template_values['customspeopleemails'] = customspeopleemails
+        for person in customspeople:
+            customspeoplerooms.append(hootdata.ROOMS[person])
+        template_values['customspeoplerooms'] = customspeoplerooms
+        #Peer mentors
+        for person in range(0,len(hootdata.BRECON_PM)):
+            peermentors.append(hootdata.BRECON_PM[person])
+        template_values['peermentors'] = peermentors
+        for person in peermentors:
+            peermentorsemails.append(hootdata.EMAILS[person])
+        template_values['peermentorsemails'] = peermentorsemails
+        for person in peermentors:
+            peermentorsrooms.append(hootdata.ROOMS[person])
+
     #PEMEAST FIRST
-    if self.request.get('dorm')=='Pembroke East' and self.request.get('floor')=='1':
+    elif self.request.get('dorm')=='Pembroke East' and self.request.get('floor')=='1':
         template_values['halladvisor'] = hootdata.PEMEAST_HA[self.request.get('floor')]
         template_values['halladvisoremail'] = hootdata.EMAILS[hootdata.PEMEAST_HA[self.request.get('floor')]]
         template_values['halladvisorroom'] = hootdata.ROOMS[hootdata.PEMEAST_HA[self.request.get('floor')]]
