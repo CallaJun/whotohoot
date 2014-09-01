721da2c93833a317d8a449de37d20977a6f63366
CallaJun <maracondo@aol.com>
2014-09-01 13:24:35 -0400

    Initial commit

---
+++ hootdata.py
@@ -0,0 +1,93 @@
+#PEMEAST
+PEMEAST_HA = {
+    '1': 'Rasha Younes',
+    '2': 'Sophia Dauria',
+    '3': 'Kailina Mastroianni'
+}
+
+
+PEMEAST_PM = [
+    'Katie Guye',
+    'Palak Bhandari'
+]
+
+PEMEAST_C1 = [
+	'Jane Rossman',
+	'Rachael Koone',
+	'Priyanka Dutta'
+]
+
+PEMEAST_C2 = [
+	'Erica Rice',
+	'Maya Berrol-Young'
+]
+
+PEMEAST_C3 = [
+	'Carol Bowe',
+	'Glynn Siobhan'
+]
+
+PEMWEST_HA = {
+    '1': 'Anna Sargeant',
+    '2': 'Akthar Hazara',
+    '3': 'Ahmad Shamial'
+}
+
+
+PEMWEST_PM = [
+    'Katie Guye',
+    'Palak Bhandari'
+]
+
+#PEMWEST
+PEMWEST_C1 = [
+	'Jane Rossman',
+	'Rachael Koone',
+	'Priyanka Dutta'
+]
+
+PEMWEST_C2 = [
+	'Erica Rice',
+	'Maya Berrol-Young'
+]
+
+PEMWEST_C3 = [
+	'Carol Bowe',
+	'Glynn Siobhan'
+]
+
+EMAILS = {
+	'Ahmad Shamial': 'siahmad@brynmawr.edu',
+	'Akthar Hazara': 'hakthar@brynmawr.edu',
+    'Anna Sargeant': 'asargeant@brynmawr.edu',
+    'Carol Bowe': 'cbowe@brynmawr.edu',
+    'Erica Rice': 'earice@brynmawr.edu',
+    'Glynn Siobhan': 'sglynn@brynmawr.edu',
+    'Jane Rossman': 'jrossman@brynmawr.edu',
+    'Kailina Mastroianni': 'kmastroian@brynmawr.edu',
+    'Katie Guye': 'kguye@brynmawr.edu',
+    'Maya Berrol-Young': 'mberrolyou@brynmawr.edu',
+    'Palak Bhandari': 'pbhandari@brynmawr.edu',
+    'Rachael Koone': 'rkoone@brynmawr.edu',
+    'Rasha Younes': 'ryounes@brynmawr.edu',
+    'Sophia Dauria': 'sdauria@brynmawr.edu',
+    'Priyanka Dutta': 'pdutta@brynmawr.edu'
+}
+
+ROOMS = {
+	'Ahmad Shamial': 'Pem West 323',
+	'Akthar Hazara': 'Pem West 207',
+	'Anna Sargeant': 'Pem West 115',
+	'Carol Bowe': 'Pem West 324',
+	'Erica Rice': 'Pem West 206',
+	'Glynn Siobhan': 'Pem West 319',
+	'Jane Rossman': 'Pem West 103',
+	'Kailina Mastroianni': 'Pem East 322',
+    'Katie Guye': 'Pem West 219',
+    'Maya Berrol-Young': 'Pem West 217',
+    'Palak Bhandari': 'Pem West 204',
+    'Rachael Koone': 'Pem West 114',
+    'Rasha Younes': 'Pem East 120',
+    'Sophia Dauria': 'Pem East 222',
+    'Priyanka Dutta': 'Pem West 107'
+}
---
+++ main.py
@@ -0,0 +1,235 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import jinja2
+import logging
+import os
+import webapp2
+import hootdata
+
+jinja_environment = jinja2.Environment(loader=
+    jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write('Hello world!')
+
+class HomeHandler(webapp2.RequestHandler):
+    def get(self):
+    	template_values = {
+        }
+        template = jinja_environment.get_template('views/home.html')
+        self.response.out.write(template.render(template_values))
+
+class HootHandler(webapp2.RequestHandler):
+  def get(self):  
+    template_values = {'name' : self.request.get('name'),
+    					'dorm' : self.request.get('dorm'),
+                    	'floor' : self.request.get('floor'),
+                       }
+
+    #PEMEAST FIRST
+    if self.request.get('dorm')=='Pembroke East' and self.request.get('floor')=='1':
+        template_values['halladvisor'] = hootdata.PEMEAST_HA[self.request.get('floor')]
+        template_values['halladvisoremail'] = hootdata.EMAILS[hootdata.PEMEAST_HA[self.request.get('floor')]]
+        template_values['halladvisorroom'] = hootdata.ROOMS[hootdata.PEMEAST_HA[self.request.get('floor')]]
+        customspeople = []
+        customspeopleemails = []
+        customspeoplerooms = []
+        peermentors = []
+        peermentorsemails = []
+        peermentorsrooms = []
+        #Custom people
+        for person in range(0,len(hootdata.PEMEAST_C1)):
+        	customspeople.append(hootdata.PEMEAST_C1[person])
+        template_values['customspeople'] = customspeople
+        for person in customspeople:
+        	customspeopleemails.append(hootdata.EMAILS[person])
+        template_values['customspeopleemails'] = customspeopleemails
+        for person in customspeople:
+        	customspeoplerooms.append(hootdata.ROOMS[person])
+        template_values['customspeoplerooms'] = customspeoplerooms
+        #Peer mentors
+        for person in range(0,len(hootdata.PEMEAST_PM)):
+        	peermentors.append(hootdata.PEMEAST_PM[person])
+        template_values['peermentors'] = peermentors
+        for person in peermentors:
+        	peermentorsemails.append(hootdata.EMAILS[person])
+        template_values['peermentorsemails'] = peermentorsemails
+        for person in peermentors:
+        	peermentorsrooms.append(hootdata.ROOMS[person])
+
+    #PEMEAST SECOND
+    elif self.request.get('dorm')=='Pembroke East' and self.request.get('floor')=='2':
+        template_values['halladvisor'] = hootdata.PEMEAST_HA[self.request.get('floor')]
+        template_values['halladvisoremail'] = hootdata.EMAILS[hootdata.PEMEAST_HA[self.request.get('floor')]]
+        template_values['halladvisorroom'] = hootdata.ROOMS[hootdata.PEMEAST_HA[self.request.get('floor')]]
+        customspeople = []
+        customspeopleemails = []
+        customspeoplerooms = []
+        peermentors = []
+        peermentorsemails = []
+        peermentorsrooms = []
+        #Custom people
+        for person in range(0,len(hootdata.PEMEAST_C2)):
+        	customspeople.append(hootdata.PEMEAST_C2[person])
+        template_values['customspeople'] = customspeople
+        for person in customspeople:
+        	customspeopleemails.append(hootdata.EMAILS[person])
+        template_values['customspeopleemails'] = customspeopleemails
+        for person in customspeople:
+        	customspeoplerooms.append(hootdata.ROOMS[person])
+        template_values['customspeoplerooms'] = customspeoplerooms
+        #Peer mentors
+        for person in range(0,len(hootdata.PEMEAST_PM)):
+        	peermentors.append(hootdata.PEMEAST_PM[person])
+        template_values['peermentors'] = peermentors
+        for person in peermentors:
+        	peermentorsemails.append(hootdata.EMAILS[person])
+        template_values['peermentorsemails'] = peermentorsemails
+        for person in peermentors:
+        	peermentorsrooms.append(hootdata.ROOMS[person])
+
+    #PEMEAST THIRD    	
+    elif self.request.get('dorm')=='Pembroke East' and self.request.get('floor')=='3':
+        template_values['halladvisor'] = hootdata.PEMEAST_HA[self.request.get('floor')]
+        template_values['halladvisoremail'] = hootdata.EMAILS[hootdata.PEMEAST_HA[self.request.get('floor')]]
+        template_values['halladvisorroom'] = hootdata.ROOMS[hootdata.PEMEAST_HA[self.request.get('floor')]]
+        customspeople = []
+        customspeopleemails = []
+        customspeoplerooms = []
+        peermentors = []
+        peermentorsemails = []
+        peermentorsrooms = []
+        #Custom people
+        for person in range(0,len(hootdata.PEMEAST_C3)):
+        	customspeople.append(hootdata.PEMEAST_C3[person])
+        template_values['customspeople'] = customspeople
+        for person in customspeople:
+        	customspeopleemails.append(hootdata.EMAILS[person])
+        template_values['customspeopleemails'] = customspeopleemails
+        for person in customspeople:
+        	customspeoplerooms.append(hootdata.ROOMS[person])
+        template_values['customspeoplerooms'] = customspeoplerooms
+        #Peer mentors
+        for person in range(0,len(hootdata.PEMEAST_PM)):
+        	peermentors.append(hootdata.PEMEAST_PM[person])
+        template_values['peermentors'] = peermentors
+        for person in peermentors:
+        	peermentorsemails.append(hootdata.EMAILS[person])
+        template_values['peermentorsemails'] = peermentorsemails
+        for person in peermentors:
+        	peermentorsrooms.append(hootdata.ROOMS[person])
+
+    #PEMWEST FIRST
+    elif self.request.get('dorm')=='Pembroke West' and self.request.get('floor')=='1':
+        template_values['halladvisor'] = hootdata.PEMWEST_HA[self.request.get('floor')]
+        template_values['halladvisoremail'] = hootdata.EMAILS[hootdata.PEMWEST_HA[self.request.get('floor')]]
+        template_values['halladvisorroom'] = hootdata.ROOMS[hootdata.PEMWEST_HA[self.request.get('floor')]]
+        customspeople = []
+        customspeopleemails = []
+        customspeoplerooms = []
+        peermentors = []
+        peermentorsemails = []
+        peermentorsrooms = []
+        #Custom people
+        for person in range(0,len(hootdata.PEMWEST_C1)):
+        	customspeople.append(hootdata.PEMWEST_C1[person])
+        template_values['customspeople'] = customspeople
+        for person in customspeople:
+        	customspeopleemails.append(hootdata.EMAILS[person])
+        template_values['customspeopleemails'] = customspeopleemails
+        for person in customspeople:
+        	customspeoplerooms.append(hootdata.ROOMS[person])
+        template_values['customspeoplerooms'] = customspeoplerooms
+        #Peer mentors
+        for person in range(0,len(hootdata.PEMWEST_PM)):
+        	peermentors.append(hootdata.PEMWEST_PM[person])
+        template_values['peermentors'] = peermentors
+        for person in peermentors:
+        	peermentorsemails.append(hootdata.EMAILS[person])
+        template_values['peermentorsemails'] = peermentorsemails
+        for person in peermentors:
+        	peermentorsrooms.append(hootdata.ROOMS[person])
+
+    #PEMWEST SECOND
+    elif self.request.get('dorm')=='Pembroke West' and self.request.get('floor')=='2':
+        template_values['halladvisor'] = hootdata.PEMWEST_HA[self.request.get('floor')]
+        template_values['halladvisoremail'] = hootdata.EMAILS[hootdata.PEMWEST_HA[self.request.get('floor')]]
+        template_values['halladvisorroom'] = hootdata.ROOMS[hootdata.PEMWEST_HA[self.request.get('floor')]]
+        customspeople = []
+        customspeopleemails = []
+        customspeoplerooms = []
+        peermentors = []
+        peermentorsemails = []
+        peermentorsrooms = []
+        #Custom people
+        for person in range(0,len(hootdata.PEMWEST_C2)):
+        	customspeople.append(hootdata.PEMWEST_C2[person])
+        template_values['customspeople'] = customspeople
+        for person in customspeople:
+        	customspeopleemails.append(hootdata.EMAILS[person])
+        template_values['customspeopleemails'] = customspeopleemails
+        for person in customspeople:
+        	customspeoplerooms.append(hootdata.ROOMS[person])
+        template_values['customspeoplerooms'] = customspeoplerooms
+        #Peer mentors
+        for person in range(0,len(hootdata.PEMWEST_PM)):
+        	peermentors.append(hootdata.PEMWEST_PM[person])
+        template_values['peermentors'] = peermentors
+        for person in peermentors:
+        	peermentorsemails.append(hootdata.EMAILS[person])
+        template_values['peermentorsemails'] = peermentorsemails
+        for person in peermentors:
+        	peermentorsrooms.append(hootdata.ROOMS[person])
+
+    #PEMWEST THIRD
+    elif self.request.get('dorm')=='Pembroke West' and self.request.get('floor')=='3':
+        template_values['halladvisor'] = hootdata.PEMWEST_HA[self.request.get('floor')]
+        template_values['halladvisoremail'] = hootdata.EMAILS[hootdata.PEMWEST_HA[self.request.get('floor')]]
+        template_values['halladvisorroom'] = hootdata.ROOMS[hootdata.PEMWEST_HA[self.request.get('floor')]]
+        customspeople = []
+        customspeopleemails = []
+        customspeoplerooms = []
+        peermentors = []
+        peermentorsemails = []
+        peermentorsrooms = []
+        #Custom people
+        for person in range(0,len(hootdata.PEMWEST_C3)):
+        	customspeople.append(hootdata.PEMWEST_C3[person])
+        template_values['customspeople'] = customspeople
+        for person in customspeople:
+        	customspeopleemails.append(hootdata.EMAILS[person])
+        template_values['customspeopleemails'] = customspeopleemails
+        for person in customspeople:
+        	customspeoplerooms.append(hootdata.ROOMS[person])
+        template_values['customspeoplerooms'] = customspeoplerooms
+        #Peer mentors
+        for person in range(0,len(hootdata.PEMWEST_PM)):
+        	peermentors.append(hootdata.PEMWEST_PM[person])
+        template_values['peermentors'] = peermentors
+        for person in peermentors:
+        	peermentorsemails.append(hootdata.EMAILS[person])
+        template_values['peermentorsemails'] = peermentorsemails
+        for person in peermentors:
+        	peermentorsrooms.append(hootdata.ROOMS[person])
+
+        template_values['peermentorsrooms'] = peermentorsrooms
+    template = jinja_environment.get_template('views/hoot.html')
+    self.response.out.write(template.render(template_values))
+
+routes = [('/', HomeHandler),('/hoot', HootHandler)]
+app = webapp2.WSGIApplication(routes, debug=True) 
